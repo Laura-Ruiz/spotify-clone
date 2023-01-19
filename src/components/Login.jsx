bdf2e7631faa2a32f016a2cf2e82e2ba3c0bd967
Laura-Ruiz <laaura.26@gmail.com>
2023-01-19 11:22:27 +0100

    spotify-clone

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,29 +2,28 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Login() {
-  const handleClick = () => {
-    const clientId = "43ab1a17d4af4d8aa4a9bc15e5898b36";
-    const redirectUrl = "https://app-spotify-clone.netlify.app/";
-    const apiUrl = "https://accounts.spotify.com/authorize";
+  const handleClick = async () => {
+    const client_id = "b10ac7cc459d474e961a6603c15da715";
+    const redirect_uri = "https://spotify-clone-api.netlify.app/";
+    const api_uri = "https://accounts.spotify.com/authorize";
     const scope = [
-      "user-read-email",
       "user-read-private",
-      "user-read-playback-state",
+      "user-read-email",
       "user-modify-playback-state",
+      "user-read-playback-state",
       "user-read-currently-playing",
-      "user-read-playback-position",
-      "user-top-read",
       "user-read-recently-played",
+      "user-top-read",
     ];
-
-    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(" ")}&response_type=token&show_daialog=true`
+    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_dialog=true`;
   };
-
   return (
     <Container>
       <img
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
-        alt="logo"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
+        alt="spotify"
       />
       <button onClick={handleClick}>Connect Spotify</button>
     </Container>
@@ -33,9 +32,9 @@ export default function Login() {
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
   height: 100vh;
   width: 100vw;
   background-color: #1db954;
@@ -46,9 +45,9 @@ const Container = styled.div`
   button {
     padding: 1rem 5rem;
     border-radius: 5rem;
-    border: none;
     background-color: black;
     color: #49f585;
+    border: none;
     font-size: 1.4rem;
     cursor: pointer;
   }
